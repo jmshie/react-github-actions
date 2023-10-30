3a2459cf32681f99cd27b43869726636b2ae2f7e
Jeremy Shields <j_m_shields@yahoo.com>
2023-10-30 19:32:11 -0400

    Add workflow to deploiy to AWS S3

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -3,6 +3,16 @@ const github = require('@actions/github')
 const exec = require('@actions/exec')
 
 const run = () => {
+  // Get input files
+  const bucketName = core.getInput('bucket-name', { required: true })
+  const bucketRegion = core.getInput('bucket-region', { required: true })
+  const distFolder = core.getInput('dist-folder', { required: true })
+
+  // Upload files
+  // NOTE: AWS CLI is installed on runners
+  const s3Uri = `s3://${bucketName}`
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+
   core.notice('Hello from my custom JS Action!')
 }
 
